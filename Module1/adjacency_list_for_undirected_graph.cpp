bdbd23230eefd1cfa03cd7f152b0f8b5e07d6777
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-02-07 21:26:44 +0600

    adjecy

---
+++ Module1/adjacency_list_for_undirected_graph.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int n,e;
+    cin>>n>>e;
+    // int a[n];
+    vector<int>adj_list[n];             // array of vector
+
+    while(e--)
+    {
+        int a,b; cin>>a>>b;
+        adj_list[a].push_back(b);      // undirected graph
+        adj_list[b].push_back(a);
+    }
+    for(int i=0;i<n; i++)
+    {
+        cout<<i<<" -> ";
+        for(int x:adj_list[i])
+        cout<<x<<" ";
+        cout<<endl;
+    }
+
+    return 0;
+}
---
+++ Module1/tempCodeRunnerFile.cpp
@@ -0,0 +1,8 @@
+        for(int i=0; i<n; i++)
+    {
+        for(int j=0; j<n; j++)
+        {
+            cout<<adj_mat[i][j]<<" ";
+        }
+        cout<<endl;
+    }
