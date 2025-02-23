458428b322ce3fa1e5d9869dd196fef16d15f2f7
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-02-23 23:15:36 +0600

    balman_ford

---
+++ Module9/bellman-ford.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Edge
+{
+public:
+    int a, b, c;
+    Edge(int a, int b, int c)
+    {
+        this->a = a;
+        this->b = b;
+        this->c = c;
+    }
+};
+int dis[1005];
+int n, e;
+vector<Edge> edj_list;
+void belman_ford()
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+
+        for (auto ed : edj_list)
+        {
+            int a = ed.a;
+            int b = ed.b;
+            int c = ed.c;
+            if (dis[a] != INT_MAX && dis[a] + c < dis[b])
+                dis[b] = dis[a] + c;
+        }
+    }
+}
+
+int main()
+{
+    cin >> n >> e;
+
+    while (e--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        edj_list.push_back(Edge(a, b, c));
+    }
+    for (int i = 0; i < n; i++)
+        dis[i] = INT_MAX;
+    dis[0] = 0;
+    belman_ford();
+    for (int i = 0; i < n; i++)
+    {
+        cout << i << " -> " << dis[i] << endl;
+    }
+
+    return 0;
+}
