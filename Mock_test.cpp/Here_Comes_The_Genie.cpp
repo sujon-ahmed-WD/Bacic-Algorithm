c6799d4bf3d53ae783f7be335b20b1936edbe9d7
sujon-ahmed-WD <sujon.mrist@gmail.com>
2025-03-15 23:24:45 +0600

    kj

--- Mock_test.cpp/Here_Comes_The_Genie.cpp
+++ Mock_test.cpp/Here_Comes_The_Genie.cpp
@@ -2,28 +2,9 @@
 using namespace std;
 int main()
 {
-    set<long long> val;
-    long long n;
-    cin >> n;
-    for (long long i = 0; i < n; i++)
-    {
-        long long x;
-        cin >> x;
-        val.insert(x);
-    }
-    long long sum = 0;
-    for (auto c : val)
-    {
-        sum += c;
-    }
-    cout << sum;
-    // int sum=0;
-    // int n; cin>>n;
-    // for(int i=0; i<n; i++)
-    // {
-    //      int x; cin>>x;
-    //      sum+=x;
-    // }
-    // cout<<sum;
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int n;cin>>n;
+    vector<int>a(n);
     return 0;
 }
